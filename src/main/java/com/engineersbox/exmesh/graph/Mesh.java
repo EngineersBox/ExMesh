bc9afd6c723d18e3703d7220f275e0a1584b3777
u6940136 <u6940136@anu.edu.au>
2023-06-22 01:20:22 +1000

    Created pipe as concurrent fixed-sized queue

--- src/main/java/com/engineersbox/exmesh/graph/Mesh.java
+++ src/main/java/com/engineersbox/exmesh/graph/Mesh.java
@@ -1,18 +1,20 @@
 package com.engineersbox.exmesh.graph;
 
 import com.engineersbox.exmesh.execution.Task;
-import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.DirectedPseudograph;
 
-public class Mesh extends DirectedPseudograph<Task<?,?,?,?>, DefaultEdge> {
+import java.util.function.Supplier;
 
-    public Mesh() {
-        super(DefaultEdge.class);
+public class Mesh<E> extends DirectedPseudograph<Task<?,?,?,?>, E> {
+
+    public Mesh(final Supplier<E> edgeSupplier,
+                final boolean weighted) {
+        super(null, edgeSupplier, weighted);
     }
 
     @Override
-    public DefaultEdge addEdge(final Task<?,?,?,?> sourceVertex,
-                               final Task<?,?,?,?> targetVertex) {
+    public E addEdge(final Task<?,?,?,?> sourceVertex,
+                     final Task<?,?,?,?> targetVertex) {
         if (!targetVertex.accepts(sourceVertex)) {
             throw new IllegalStateException(String.format(
                     "Cannot connect vertices with incompatible type bounds:\n\t[SINGLE | SINGLE] %s => %s\n\t[SINGLE | COLLECTION] %s => %s\n\t[COLLECTION | COLLECTION] %s => %s",
