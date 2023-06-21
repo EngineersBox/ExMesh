76101e0eb4b26a2234529d28acc362f3a2358343
u6940136 <u6940136@anu.edu.au>
2023-06-22 01:24:36 +1000

    Created pipe as concurrent fixed-sized queue

--- src/main/java/com/engineersbox/exmesh/allocation/Allocator.java
+++ src/main/java/com/engineersbox/exmesh/allocation/Allocator.java
@@ -1,7 +1,14 @@
 package com.engineersbox.exmesh.allocation;
 
+import com.engineersbox.exmesh.graph.Mesh;
+
 public interface Allocator {
 
-
+    /**
+     * Allocate resources to mesh nodes according to colouring.
+     * @param mesh Mesh instance to reference
+     * @param <E> Edge type
+     */
+    <E> void allocate(final Mesh<E> mesh);
 
 }
--- src/main/java/com/engineersbox/exmesh/execution/type/Compatibility.java
+++ src/main/java/com/engineersbox/exmesh/execution/type/Compatibility.java
@@ -5,8 +5,18 @@ import com.google.common.reflect.TypeToken;
 @FunctionalInterface
 public interface Compatibility<T> {
 
+    /**
+     * Determine whether a given value of type (from) can be passed to a recipient expecting a value of type to
+     * @param from Source value type
+     * @param to Recipient expected type
+     * @return Whether the types are compatible (true) or not (false)
+     */
     boolean check(final TypeToken<?> from, final TypeToken<T> to);
 
+    /**
+     * Standard implementation of compatibility check, equality and then supertype bounds
+     * @param <T> Recipient expected type
+     */
     class Default<T> implements Compatibility<T> {
 
         @Override
