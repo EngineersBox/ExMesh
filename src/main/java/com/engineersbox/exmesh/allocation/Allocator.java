8b7a8075e5f0ab6bc38e36824df0b0d88198bc65
u6940136 <u6940136@anu.edu.au>
2023-06-23 00:03:08 +1000

    Clarified allocator behaviour

--- src/main/java/com/engineersbox/exmesh/allocation/Allocator.java
+++ src/main/java/com/engineersbox/exmesh/allocation/Allocator.java
@@ -5,7 +5,7 @@ import com.engineersbox.exmesh.graph.Mesh;
 public interface Allocator {
 
     /**
-     * Allocate resources to mesh nodes according to colouring.
+     * Allocate resources to mesh nodes according to colouring and topological sorting.
      * @param mesh Mesh instance to reference
      * @param <E> Edge type
      */
