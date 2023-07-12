00d3e59f56665ea0dd60bdf528d0df7b3b1b4288
EngineersBox <toastcraft.info@gmail.com>
2023-07-12 23:02:54 +1000

    Added handlers for full lifecyle of resource

--- src/main/java/com/engineersbox/exmesh/resource/AllocatableResource.java
+++ src/main/java/com/engineersbox/exmesh/resource/AllocatableResource.java
@@ -7,9 +7,32 @@ import java.util.function.Function;
 
 public interface AllocatableResource extends Function<Task<?,?,?,?>, ExecutionResult> {
 
+    /**
+     * Invoked during initial allocation
+     */
+    void startup();
+
+    /**
+     * Invoked before task is to be run on it
+     */
     void configure();
 
+    /**
+     * Handler to perform a given task on the resource
+     * @param task the task to be executed
+     * @return result of the execution of the task
+     */
     @Override
     ExecutionResult apply(final Task task);
+
+    /**
+     * Invoked after task has been run on it
+     */
+    void cleanup();
+
+    /**
+     * Invoked during deallocation from resource pool
+     */
+    void teardown();
 
 }
--- src/main/java/com/engineersbox/exmesh/resource/ResourceFactory.java
+++ src/main/java/com/engineersbox/exmesh/resource/ResourceFactory.java
@@ -2,8 +2,17 @@ package com.engineersbox.exmesh.resource;
 
 public interface ResourceFactory<T extends AllocatableResource> {
 
-    T provision();
+    /**
+     * Create a new allocatable resource
+     * @return Resource instance
+     */
+    T provision() throws Exception;
 
-    Iterable<T> provision(final int count);
+    /**
+     * Create a number of allocatable resources
+     * @param count Number of resources to allocate
+     * @return Instantiated resources
+     */
+    Iterable<T> provision(final int count) throws Exception;
 
 }
