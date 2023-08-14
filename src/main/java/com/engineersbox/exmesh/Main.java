ff8ef64565b203653c8d29b771b738d5904904bd
EngineersBox <toastcraft.info@gmail.com>
2023-08-14 23:19:55 +1000

    Created example complex mesh to test colouring and clustering

--- src/main/java/com/engineersbox/exmesh/Main.java
+++ src/main/java/com/engineersbox/exmesh/Main.java
@@ -30,6 +30,7 @@ import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
 
@@ -283,8 +284,68 @@ public class Main {
         return mesh;
     }
 
+    private static final class OTask extends Task<Object, Object, Object, Object> {
+
+        public OTask(final int i) {
+            super(String.valueOf(i), 1.0);
+        }
+
+        @Override
+        public void invoke() {
+
+        }
+
+        @Override
+        public Object consolidateSingleton(@Nonnull Iterable<Object> values) {
+            return null;
+        }
+
+        @Override
+        public Object consolidateCollection(@Nonnull Iterable<Object> collections) {
+            return null;
+        }
+
+        @Nonnull
+        @Override
+        public Object splitSingleton() {
+            return null;
+        }
+
+        @Nonnull
+        @Override
+        public Object splitCollection(int partitionCount) {
+            return null;
+        }
+
+        @Override
+        public int internalCollectionSize() {
+            return 0;
+        }
+    }
+
+    private static Mesh<Pipe> exampleMesh() {
+        final Mesh<Pipe> mesh = new Mesh<>(() -> new Pipe(5));
+        final OTask[] tasks = new OTask[11];
+        IntStream.range(0, tasks.length).forEach((final int i) -> mesh.addVertex(tasks[i] = new OTask(i)));
+        mesh.addEdgeS2S(tasks[0], tasks[1]);
+        mesh.addEdgeS2S(tasks[0], tasks[2]);
+        mesh.addEdgeS2S(tasks[1], tasks[6]);
+        mesh.addEdgeS2S(tasks[1], tasks[7]);
+        mesh.addEdgeS2S(tasks[2], tasks[3]);
+        mesh.addEdgeS2S(tasks[2], tasks[4]);
+        mesh.addEdgeS2S(tasks[2], tasks[5]);
+        mesh.addEdgeS2S(tasks[3], tasks[7]);
+        mesh.addEdgeS2S(tasks[4], tasks[7]);
+        mesh.addEdgeS2S(tasks[6], tasks[8]);
+        mesh.addEdgeS2S(tasks[5], tasks[9]);
+        mesh.addEdgeS2S(tasks[8], tasks[10]);
+        mesh.addEdgeS2S(tasks[7], tasks[10]);
+        mesh.addEdgeS2S(tasks[9], tasks[10]);
+        return mesh;
+    }
+
     public static void main(final String[] args) {
-        final Mesh<Pipe> mesh = createTestMesh();
+        Mesh<Pipe> mesh = createTestMesh();
 //        final Scheduler warpInterleavedScheduler = new WarpInterleavedScheduler(
 //                4,
 //                2,
@@ -302,13 +363,14 @@ public class Main {
         executor.submit(mesh);
         executor.run();
 
+        mesh = exampleMesh();
         final BrownBacktrackColoring<Task<?,?,?,?>,? extends Pipe> coloring = new BrownBacktrackColoring<>(mesh);
         coloring.getColoring().getColors().forEach((final Task<?,?,?,?> task, final Integer color) -> LOGGER.info(
                 "Task: {} | Color: {}",
                 task.getName(),
                 color
         ));
-        final GirvanNewmanClustering<Task<?,?,?,?>, ? extends Pipe> clustering = new GirvanNewmanClustering<>(mesh, 3);
+        final GirvanNewmanClustering<Task<?,?,?,?>, ? extends Pipe> clustering = new GirvanNewmanClustering<>(mesh, 4);
         final MutableInt count = new MutableInt(0);
         clustering.getClustering()
                 .forEach((final Set<Task<?,?,?,?>> cluster) -> {
